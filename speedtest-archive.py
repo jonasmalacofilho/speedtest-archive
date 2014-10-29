316a338518938945fac105ee5cfbdcc0fbf5ccad
Jonas Malaco Filho <jonas@jonasmalaco.com>
2014-10-29 19:59:34 -0200

    Interface and gateway name/address filters
    The purpose of this is to decrease the need to manually disable the
    cronjobs on some networks (like hotel WiFi or 3G/4G).
    
    For now, this only covers the simplest use case, that of someone
    monitoring one (or more) specific connections.

--- speedtest-archive.py
+++ speedtest-archive.py
@@ -1,13 +1,33 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Copyright 2014 Jonas Malaco Filho
-# Licensed under the Modified BSD (3-clause) License
+
+"""
+Speedtest measurement archiver.
+
+Usage:
+    speedtest-archive [--iface=<name>] [--gatename=<name>] [--gatemac=<addr>]
+    speedtest-archive -h | --help
+    speedtest-archive --version
+
+Options:
+    -i <name>, --iface=<name>     Only run if on interface <name>
+    -n <name>, --gatename=<name>  Only run if the default gateway name matches
+                                  <name>
+    -a <addr>, --gatemac=<addr>   Only run if the default gateway MAC address
+                                  matches <addr>
+
+<https://github.com/jonasmalacofilho/speedtest-archive>
+Copyright 2014 Jonas Malaco Filho.
+Licensed under the Modified BSD (3-clause) License.
+"""
 
 from __future__ import print_function
 from datetime import datetime
+from docopt import docopt
 from minifixed import reader as fixedreader
 from subprocess import Popen, PIPE
 import json
+import sys
 
 
 def get_routing_table():
@@ -50,16 +70,38 @@ def speedtest():
     return json.loads(proc.stdout.read())
 
 
+version = "0.1.2"
+
+
 if __name__ == "__main__":
-    time = datetime.now().isoformat()
+
+    args = docopt(__doc__)
+    if args["--help"] or args["-h"]:
+        print(__doc__)
+        sys.exit(0)
+    if args["--version"]:
+        print(version)
+        sys.exit(0)
+
+    # get infos
     info = get_gateway_info()
+    if args["--iface"] and args["--iface"] != info["interface"]:
+        sys.exit(21)
+    if args["--gatename"] and args["--gatename"] != info["gateway_name"]:
+        sys.exit(22)
+    if args["--gatemac"] and args["--gatemac"] != info["gateway_mac"]:
+        sys.exit(23)
+
+    # collect data
+    time = datetime.now().isoformat()
     res = speedtest()
 
+    # output
     out = [
         time,
         info["interface"], info["gateway_name"], info["gateway_mac"],
         str(res["ping"]), str(res["download"]), str(res["upload"]),
         str(res["server"]), res["share"]
     ]
-
     print("\t".join(out))
+
